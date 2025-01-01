a70bed6114e56719b2563e02708a3e1663f091ec
viraj chelamkuri <126386413+Virajchelamkuri@users.noreply.github.com>
2025-01-02 00:13:30 +0530

    Update db.php

--- db.php
+++ db.php
@@ -6,6 +6,7 @@ $database = 'dreamdraft';
 
 $conn = new mysqli($host, $username, $password, $database);
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
