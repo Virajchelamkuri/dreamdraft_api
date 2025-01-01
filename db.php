9f41daad1273a6a4c86d07a724fa35d33da1e5f7
viraj chelamkuri <126386413+Virajchelamkuri@users.noreply.github.com>
2025-01-02 00:28:28 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 // Database connection details
-$host = getenv('DB_HOST') ?: 'localhost';  // Use DB_HOST environment variable or fallback to localhost
+$host = getenv('DB_HOST') ?: '127.0.0.1';  // Use DB_HOST environment variable or fallback to localhost
 $username = getenv('DB_USER') ?: 'root';   // Use DB_USER environment variable or fallback to root
 $password = getenv('DB_PASSWORD') ?: '';   // Use DB_PASSWORD environment variable or fallback to empty
 $database = getenv('DB_NAME') ?: 'dreamdraft';  // Use DB_NAME environment variable or fallback to 'dreamdraft'
